b139863935503f03a95f73969bee25ba987a8d45
Vyas Ramankulangara <vyas01890@outlook.com>
2022-07-26 23:47:27 -0500

    remove useEffect

--- src/screens/manage-members/manage-members.screen.tsx
+++ src/screens/manage-members/manage-members.screen.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import BasicTable from "../../components/basic-table";
 import { Toolbar } from "primereact/toolbar";
